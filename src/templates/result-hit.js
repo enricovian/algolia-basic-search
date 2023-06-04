9b5d905a8178a6fe1cb510231125ca59600955a3
Enrico Vianello <vianello94@gmail.com>
2023-06-04 13:38:43 +0200

    Highlight matches in product names

--- src/templates/result-hit.js
+++ src/templates/result-hit.js
@@ -3,7 +3,7 @@ const resultHit = (hit, { html, components, sendEvent }) => html`<div class="res
     <img class="result-hit__image" src="${hit.image}" />
   </div>
   <div class="result-hit__details">
-    <h3 class="result-hit__name">${hit._highlightResult.name.value}</h3>
+    <h3 class="result-hit__name">${components.Highlight({ hit, attribute: 'name' })}</h3>
     <p class="result-hit__price">$${hit.price}</p>
   </div>
   <div class="result-hit__controls">
